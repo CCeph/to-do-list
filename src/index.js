3de784f23550cebaaaa1b9072be2df894d5fd0ff
Yousef <yousefelsisyy@gmail.com>
2023-03-17 18:21:32 -0300

    Create DOM Cache

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import "./style.css";
 
+function createDOMCache() {
+  const $headerAddTaskButton = document.querySelector(".add-task");
+  const $cancelNewTaskButton = document.querySelector(
+    ".addTaskPopup button[type=reset]"
+  );
+  const $submitNewTaskButton = document.querySelector(
+    "addTaskPopup button[type=submit]"
+  );
+  return { $headerAddTaskButton, $cancelNewTaskButton, $submitNewTaskButton };
+}
+
+const cachedDOM = createDOMCache();
+
+function bindEventsForAddingTasks() {}
+
 console.log("Test");
